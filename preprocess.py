9eb71e55fe471668dc7651bc325b14640eb99c01
anuta-mukherjee <manuta94@gmail.com>
2017-03-17 19:10:47 -0400

    Added sentence tokenization & parsing
    BerkeleyParser.py : python wrapper for the Berkeley Parser
    preprocess.py : Takes path to data folder as argument. The data/ folder has 4 further subfolders (set1, set2..). The code reads all text files from those subfolders and prints each sentence, followed by the parse tree for that sentence.
    
    Usage:
    preprocess.py ../data/

---
+++ preprocess.py
@@ -0,0 +1,43 @@
+"""
+
+This code tokenizes the input data into sentences, then feeds each
+sentence to the Berkeley Parser.
+OUTPUT:
+sentence
+parsed sentence
+
+"""
+
+import os
+import sys
+from BerkeleyParser import *
+from nltk.corpus import treebank
+import nltk
+import codecs
+from nltk.tokenize import sent_tokenize, word_tokenize
+
+root_dir = sys.argv[1]
+files = []
+set1 = "set1"
+set2 = "set2"
+set3 = "set3"
+set4 = "set4"
+
+#Open the subfolder set1 and read all the .txt files in it
+path1 = os.path.join(root_dir,set1)
+for i in os.listdir(path1):
+    if i.endswith('.txt'):
+	files.append(codecs.open(os.path.join(path1,i),'r'))
+
+i = 0
+for f in files:
+	lines = []
+	text = f.read()
+	text = text.decode('utf-8')
+    	sents = sent_tokenize(text)
+	break
+
+
+for line in sents:
+	print line
+	b_parse(line)
