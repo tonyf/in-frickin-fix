cb65b39e4269892bfc210649417b8ce91322f128
Anuta Mukherjee <anutam@andrew.cmu.edu>
2017-03-19 20:24:19 -0400

    WIP: pattern matching

--- pattern_generator.py
+++ pattern_generator.py
@@ -24,10 +24,22 @@ def generate_questions():
         text = f.read()
         text = text.decode('utf-8')
         sents = sent_tokenize(text)
-        break
+        for line in sents:
+            if line.strip():
+                #Pattern 1: X is Y
+                matchObj = re.match( r'(.*) is (.*? .*)', line, re.M|re.I)
+                if matchObj:
+                    X = matchObj.group(1)
+                    Y = matchObj.group(2)
+                    print "Is "+X+" "+Y+"?\n"
+                #Pattern 2: X went to Y
+                matchObj = re.match( r'(.*) went to (.*? .*)', line, re.M|re.I)
+                if matchObj:
+                    X = matchObj.group(1)
+                    Y = matchObj.group(2)
+                    print "Did "+X+" go to "+Y+"?\n"
 
-    for line in sents:
-        print line
+        break #Doing it on one file only for test purposes
 
 def generate_patterns():
     pass
