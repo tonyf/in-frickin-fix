d8f9475702f6ef398ecf6c657acf23ef3605c05d
Tony Francis <tonyf@andrew.cmu.edu>
2017-04-20 16:12:51 -0400

    windowed distance

--- question_answering/doc_utils.py
+++ question_answering/doc_utils.py
@@ -26,8 +26,9 @@ def get_doc_matrix(doc):
 
 def get_sentence_matrix(s):
     n = len(s)
-    matrix = np.zeros((EMBEDDING_SIZE, 1))
-    matrix[:,0] = s.vector
+    matrix = np.zeros((EMBEDDING_SIZE, n))
+    for i in range(n):
+        matrix[:,i] = s[i].vector
     return matrix
 
 def ent2categories(entity):
@@ -41,24 +42,28 @@ def get_category_docs(doc_dict, categories):
     return docs
 
 def compute_dist(a, b):
-    return 0.5 * windowed_dist(a, b, 1) + 0.5 * windowed_dist(a, b, 3)
+    return windowed_vector(a, b, 1)
 
-def windowed_dist(a, b, window):
+def windowed_vector(a, b, window):
     total = 0
-    normalize_factor = 0
+    a_norm = 0
+    b_norm = 0
+
+    a_sum = np.zeros(EMBEDDING_SIZE)
+    b_sum = np.zeros(EMBEDDING_SIZE)
+
     for i in xrange(0, a.shape[1], window):
         for j in xrange(0, b.shape[1], window):
-            a_sum = a[:, i:i+window].sum(axis=1)
-            b_sum = b[:, j:j+window].sum(axis=1)
-            normalize_factor += 1
+            a_sum += a[:, i:i+window].sum(axis=1)
+            b_sum += b[:, j:j+window].sum(axis=1)
 
-            if a_sum.sum() == 0:
-                total += np.linalg.norm(b[:,j])
-            elif b_sum.sum() == 0:
-                total += np.linalg.norm(b[:,j])
-            else:
-                total += sp.distance.cosine(a_sum.flatten(), b_sum.flatten())
-    return total / normalize_factor
+            if a_sum.sum() != 0:
+                a_norm += 1
+            if b_sum.sum() != 0:
+                b_norm += 1
+    a_sum = a_sum / a_norm
+    b_sum = b_sum / b_norm
+    return sp.distance.cosine(a_sum.flatten(), b_sum.flatten())
 
 def get_vector(a, window):
     total = 0
